fae6169f1768bbd60529d8a10990e17374cb3d36
someonempty <someonempty@gmail.com>
2024-04-23 17:58:45 +0300

    Adding Auth Routes And Controllers.

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,17 @@ import express from 'express';
 import authRoutes from './routes/auth.routes.js';
 import dotenv from 'dotenv';
 import connectMongoDB from './db/connectMongoDB.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 500;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 
